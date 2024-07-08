6dabdc37fb21a2283ffb52a69e60954a4fc2fb53
Jacob <balanejacob1@gmail.com>
2024-07-08 20:19:19 +0800

    feat: created beta branch

--- src/components/organisms/HeroSection.tsx
+++ src/components/organisms/HeroSection.tsx
@@ -6,49 +6,7 @@ const Hero = () => {
     <div className="flex flex-col justify-center h-screen lg:w-3/4 lg:border-x lg:border-neutral-400 px-16 py-6 md:items-center">
       <div className="grow flex items-center">
         <p className="h-2/5 lg:h-3/5 font-montserrat text-3xl md:text-6xl lg:text-7xl text-pretty font-bold text-center whitespace-pre-line">
-          I'm <span className="text-yellow-500">Jacob</span>.{" "}
-          <br className="md:hidden lg:block xl:hidden" /> I design and build web
-          applications <br className="hidden lg:block xl:hidden" /> for{" "}
-          <br className="md:block lg:hidden" />{" "}
-          <span className="text-transparent from-pink-600 to-violet-600 bg-gradient-to-r bg-clip-text ">
-            <TypeAnimation
-              sequence={[
-                "learning.",
-                5000,
-                "fun.",
-                5000,
-                "work.",
-                5000,
-                "growth.",
-                5000,
-                "community.",
-                5000,
-                "impact.",
-                5000,
-                "change.",
-                5000,
-                "progress.",
-                5000,
-                "success.",
-                5000,
-                "productivity.",
-                5000,
-                "innovation.",
-                5000,
-                "creativity.",
-                5000,
-                "passion.",
-                5000,
-                "people.",
-                5000,
-                "the future.",
-                5000,
-              ]}
-              repeat={Infinity}
-              cursor={false}
-              deletionSpeed={90}
-            />
-          </span>
+          this is beta
         </p>
       </div>
       <div className="h-2/5 flex justify-center">
