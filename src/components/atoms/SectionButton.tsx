2ba0ec46aac40e155c5f6f24a2a20ed68fe78be0
Jacob <balanejacob1@gmail.com>
2024-07-10 01:27:25 +0800

    fix: adjusted button width

--- src/components/atoms/SectionButton.tsx
+++ src/components/atoms/SectionButton.tsx
@@ -8,7 +8,7 @@ interface SectionButtonProps {
 const SectionButton: React.FC<SectionButtonProps> = ({ label }) => {
   return (
     <div className="flex justify-center items-center w-full lg:w-2/6 lg:h-full mt-8 mb-12">
-      <button className="flex justify-center items-center space-x-3 px-5 md:px-8 lg:px-10 xl:px-12 py-3 lg:py-5 text-2xs md:text-lg xl:text-2xl font-medium bg-primary text-secondary rounded-full shadow-md">
+      <button className="w-40 flex justify-center items-center space-x-3 px-5 md:px-8 lg:px-10 xl:px-12 py-3 lg:py-5 text-2xs md:text-lg xl:text-2xl font-medium bg-primary text-secondary rounded-full shadow-md">
         <p>{label}</p>
         <img src={arrow} alt="" className="h-3 md:h-5 lg:h-6 xl:h-8" />
       </button>
