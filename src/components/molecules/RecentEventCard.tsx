d4ea83e05633102480e6954461fb69ceb7660158
Jacob <balanejacob1@gmail.com>
2024-07-10 19:00:50 +0800

    fix: event instance's height increased

--- src/components/molecules/RecentEventCard.tsx
+++ src/components/molecules/RecentEventCard.tsx
@@ -14,7 +14,7 @@ const RecentEventCard: React.FC<RecentEventCardProps> = ({ event }) => {
   return (
     <div className="md:w-1/2 xl:w-1/4">
       <div className="flex flex-col font-montserrat space-y-4 md:m-2">
-        <div className="md:h-40 md:flex md:flex-col border-2 border-primary rounded-lg px-5 py-3 md:px-6 md:py-4 space-y-2">
+        <div className="md:h-40 xl:h-44 md:flex md:flex-col border-2 border-primary rounded-lg px-5 py-3 md:px-6 md:py-4 space-y-2">
           <p className="font-bold text-xs md:text-base xl:text-lg">
             {event.name}
           </p>
--- src/components/molecules/UpcomingEventCard.tsx
+++ src/components/molecules/UpcomingEventCard.tsx
@@ -15,7 +15,7 @@ const UpcomingEventCard: React.FC<UpcomingEventCardProps> = ({ event }) => {
   return (
     <div className="md:w-1/2 xl:w-1/4">
       <div className="flex flex-col font-montserrat space-y-4 md:m-2">
-        <div className="md:h-40 md:flex md:flex-col  bg-primary text-accent-2 rounded-lg px-5 py-3 md:px-6 md:py-4 space-y-2">
+        <div className="md:h-40 xl:h-44 md:flex md:flex-col  bg-primary text-accent-2 rounded-lg px-5 py-3 md:px-6 md:py-4 space-y-2">
           <p className="text-secondary font-semibold text-xs md:text-base xl:text-lg">
             {event.name}
           </p>
