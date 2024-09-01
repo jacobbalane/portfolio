037cb4c0a42c63ecc0618ea23ad72a23d467d44a
Jacob <balanejacob1@gmail.com>
2024-09-01 18:08:40 +0800

    [fix] add header max width

--- src/app/components/organisms/header.tsx
+++ src/app/components/organisms/header.tsx
@@ -9,24 +9,26 @@ interface HeaderProps {
 export default function Header({ offset }: HeaderProps) {
   return (
     <div
-      className={`sticky top-0 overflow-hidden z-10 ${
+      className={`sticky top-0 overflow-hidden z-10 bg-primary px-8 ${
         offset > 0 ? "border-b border-neutral-800" : ""
       }`}>
-      <div className="flex my-auto items-center justify-between h-28 px-8 bg-primary ">
-        <div className="flex items-center space-x-3">
-          <Image
-            src={Logo}
-            alt="Portfolio Logo"
-            className="rounded-full"
-            width={30}
-            priority
-          />
-          <p className="font-semibold text-sm text-secondary opacity-65">
-            jacob<span className="opacity-30">balane</span>
-          </p>
-        </div>
-        <div className="flex-col">
-          <Hamburger />
+      <div className="mx-auto  max-w-96">
+        <div className="flex my-auto items-center justify-between h-28">
+          <div className="flex items-center space-x-3">
+            <Image
+              src={Logo}
+              alt="Portfolio Logo"
+              className="rounded-full"
+              width={30}
+              priority
+            />
+            <p className="font-semibold text-sm text-secondary opacity-65">
+              jacob<span className="opacity-30">balane</span>
+            </p>
+          </div>
+          <div className="flex-col">
+            <Hamburger />
+          </div>
         </div>
       </div>
     </div>
