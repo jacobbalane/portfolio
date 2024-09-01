07586f43229ca86c8cdc7c30b5f3489ad506094c
Jacob <balanejacob1@gmail.com>
2024-09-01 12:21:28 +0800

    [feat] remove files

--- src/app/components/atoms/hamburger.tsx
+++
@@ -1,8 +0,0 @@
-export default function Hamburger() {
-  return (
-    <div className="flex flex-col items-center justify-around w-8 h-5 cursor-pointer">
-      <div className="w-8 h-0.5 bg-secondary opacity-87"></div>
-      <div className="w-8 h-0.5 bg-secondary opacity-87"></div>
-    </div>
-  );
-}
