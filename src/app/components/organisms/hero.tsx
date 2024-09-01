b11c7139577cd2bb93847b6b5fe7de97b79893d9
Jacob <balanejacob1@gmail.com>
2024-09-01 17:27:11 +0800

    [fix] improve text ui

--- src/app/components/organisms/hero.tsx
+++ src/app/components/organisms/hero.tsx
@@ -1,12 +1,35 @@
+import { FaDrawPolygon, FaCode } from "react-icons/fa";
+import { VscSparkleFilled } from "react-icons/vsc";
+
 export default function Hero() {
   return (
     <div className="flex items-center justify-center px-8 py-14 bg-primary">
       <div className="text-secondary opacity-87 space-y-20">
         <div className="max-w-96 space-y-4">
           <p className="text-xl opacity-65 font-medium">Hello! I’m Jacob.</p>
-          <p className="text-5xl opacity-87 font-bold">
-            I design and build web applications for work — each crafted with
-            passion.
+          <p className=" text-4xl opacity-87 font-bold leading-tight">
+            I{" "}
+            <span className="inline-flex text-pink-500">
+              <span className="flex space-x-1 items-center">
+                <span className="italic font-normal">design</span>
+                <FaDrawPolygon className="text-[25px]" />
+              </span>
+            </span>{" "}
+            and{" "}
+            <span className="inline-flex text-cyan-400 border-2 border-cyan-400 rounded-md">
+              <span className="flex space-x-1 px-2 items-center">
+                <span>build</span>
+                <FaCode className="text-[25px]" />
+              </span>
+            </span>{" "}
+            web applications for fun{" "}
+            <span className="opacity-50">— each crafted with</span>{" "}
+            <span className="inline-flex text-yellow-300">
+              <span className="flex space-x-1 items-center">
+                <span>passion</span>
+                <VscSparkleFilled className="text-[25px]" />
+              </span>
+            </span>{" "}
           </p>
         </div>
         <div className="max-w-96 space-y-4">
