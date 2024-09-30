7a4da9f9706a5f08f560a5b70326c4abd5197c30
Jacob <balanejacob1@gmail.com>
2024-10-01 01:10:46 +0800

    [fix] edge icons adjusted

--- src/app/components/atoms/EdgeIcon.tsx
+++ src/app/components/atoms/EdgeIcon.tsx
@@ -1,17 +1,21 @@
 export default function EdgeIcon() {
   return (
     <>
-      <div className="h-[1.5px] w-6 bg-accent1 absolute rotate-90 -left-3 -top-[1.5px]"></div>
-      <div className="h-[1.5px] w-6 bg-accent1 absolute -left-3 -top-[1.5px]"></div>
+      {/* top left */}
+      <div className="h-[1.5px] w-6 bg-accent1 absolute rotate-90 -left-3 -top-[1.5px] z-10"></div>
+      <div className="h-[1.5px] w-6 bg-accent1 absolute -left-3 -top-[1px] z-10"></div>
 
-      <div className="h-[1.5px] w-6 bg-accent1 absolute rotate-90 -right-3 -top-[1.5px]"></div>
-      <div className="h-[1.5px] w-6 bg-accent1 absolute -right-3 -top-[1.5px]"></div>
+      {/* top right */}
+      <div className="h-[1.5px] w-6 bg-accent1 absolute rotate-90 -right-3 -top-[1.5px] z-10"></div>
+      <div className="h-[1.5px] w-6 bg-accent1 absolute -right-3 -top-[1px] z-10"></div>
 
-      <div className="h-[1.5px] w-6 bg-accent1 absolute rotate-90 -right-3 -bottom-[1.5px]"></div>
-      <div className="h-[1.5px] w-6 bg-accent1 absolute -right-3 -bottom-[1.5px]"></div>
+      {/* bottom right */}
+      <div className="h-[1.5px] w-6 bg-accent1 absolute rotate-90 -right-3 -bottom-[1.5px] z-10"></div>
+      <div className="h-[1.5px] w-6 bg-accent1 absolute -right-3 -bottom-[.8px] z-10"></div>
 
-      <div className="h-[1.5px] w-6 bg-accent1 absolute rotate-90 -left-3 -bottom-[1.5px]"></div>
-      <div className="h-[1.5px] w-6 bg-accent1 absolute -left-3 -bottom-[1.5px]"></div>
+      {/* bottom left */}
+      <div className="h-[1.5px] w-6 bg-accent1 absolute rotate-90 -left-3 -bottom-[1.5px] z-10"></div>
+      <div className="h-[1.5px] w-6 bg-accent1 absolute -left-3 -bottom-[0.8px] z-10"></div>
     </>
   );
 }
